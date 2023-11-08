e26cd1daf1c90cf793bb9c2b70128e370d014d8f
Caio Antonio <caio.antonio.c@outlook.com>
2023-11-07 22:14:53 -0300

    first commit

--- src/main/java/tech/kafkastreamconcept/configuration/KafkaConfig.java
+++ src/main/java/tech/kafkastreamconcept/configuration/KafkaConfig.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Properties;
-import java.util.UUID;
 
 @Configuration
 public class KafkaConfig {
