1abeae15fcf81f8a853b02f9e9929e0d366028c3
Caio Antonio <caio.antonio.c@outlook.com>
2023-11-08 21:32:33 -0300

    first commit

--- src/main/java/tech/kafkastreamconcept/configuration/KafkaConfig.java
+++ src/main/java/tech/kafkastreamconcept/configuration/KafkaConfig.java
@@ -1,37 +1,28 @@
 package tech.kafkastreamconcept.configuration;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.streams.KafkaStreams;
-import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.Topology;
-import org.apache.kafka.streams.processor.WallclockTimestampExtractor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
+import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration;
+import org.springframework.kafka.config.KafkaStreamsConfiguration;
 
-import java.util.Properties;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
+@EnableKafkaStreams
 public class KafkaConfig {
 
-    @Autowired
-    private Topology streamsTopology;
+    @Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
+    KafkaStreamsConfiguration kStreamsConfigs(KafkaProperties kafkaProperties) {
+        Map<String, Object> props = new HashMap<>();
+        props.putAll(kafkaProperties.buildAdminProperties());
+        props.putAll(kafkaProperties.buildStreamsProperties());
+        props.putAll(kafkaProperties.buildConsumerProperties());
+        props.putAll(kafkaProperties.buildProducerProperties());
 
-    @Bean
-    KafkaStreams kStreamsConfigs(final KafkaProperties kafkaProperties) {
-        var props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafka-stream-concept");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
-        props.put(StreamsConfig.NUM_STREAM_THREADS_CONFIG, 1);
-        props.put(ConsumerConfig.FETCH_MAX_WAIT_MS_CONFIG, 9000);
-        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 5000);
-        props.put(StreamsConfig.DEFAULT_TIMESTAMP_EXTRACTOR_CLASS_CONFIG, WallclockTimestampExtractor.class.getName());
-
-        final var topology = new KafkaStreams(streamsTopology, props);
-        topology.start();
-
-        return topology;
+        return new KafkaStreamsConfiguration(props);
     }
 
 }
--- src/main/java/tech/kafkastreamconcept/processing/TopologyStream.java
+++ src/main/java/tech/kafkastreamconcept/processing/TopologyStream.java
@@ -2,16 +2,15 @@ package tech.kafkastreamconcept.processing;
 
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.Produced;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@Configuration
+@Component
 public class TopologyStream {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(TopologyStream.class);
@@ -25,14 +24,8 @@ public class TopologyStream {
         this.outParkingLotTopic = outParkingLotTopic;
     }
 
-    @Bean
-    public Topology topology() {
-        final var builder = new StreamsBuilder();
-        this.consumeEvent(builder);
-        return builder.build();
-    }
-
-    void consumeEvent(StreamsBuilder builder) {
+    @Autowired
+    void consumeEvent(@Autowired StreamsBuilder builder) {
         builder.stream(inEventTopic, Consumed.with(Serdes.String(), Serdes.String()))
                 .peek((k, message) -> LOGGER.info("Received message [{}]", message))
                 .to(outParkingLotTopic, Produced.with(Serdes.String(), Serdes.String()));
