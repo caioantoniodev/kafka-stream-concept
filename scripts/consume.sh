e7c2b49dd5ce857161614dd818f9806b53d49ee3
Caio Antonio <caio.antonio.c@outlook.com>
2023-12-29 10:55:12 -0300

    improvements

--- scripts/consume.sh
+++ scripts/consume.sh
@@ -1,3 +1,7 @@
 ./kafka-console-consumer.sh \
 --topic PARKING_LOT \
 --bootstrap-server localhost:9092 | jq
+
+#inside container
+./kafka-console-consumer --bootstrap-server kafka:29092 --topic INBOUND_EVENT --from-beginning
+./kafka-console-consumer --bootstrap-server kafka:29092 --topic PARKING_LOT --from-beginning
--- scripts/produce.sh
+++ scripts/produce.sh
@@ -1,1 +1,4 @@
 echo '{"message": "Hello World"}' | ./kafka-console-producer.sh --bootstrap-server localhost:9092 --topic INBOUND_EVENT
+
+#inside container
+echo '{"message": "Hello World"}' | ./kafka-console-producer --bootstrap-server kafka:9092 --topic INBOUND_EVENT
