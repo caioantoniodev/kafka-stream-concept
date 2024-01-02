e231016e1773319e7370194f6fdd26b58fc41413
Caio Antonio <caio.antonio.c@outlook.com>
2024-01-02 20:44:26 -0300

    improvements

--- scripts/consume.sh
+++ scripts/consume.sh
@@ -1,7 +1,8 @@
 ./kafka-console-consumer.sh \
---topic PARKING_LOT \
+--topic EVEN_TOPIC \
 --bootstrap-server localhost:9092 | jq
 
 #inside container
-./kafka-console-consumer --bootstrap-server kafka:29092 --topic INBOUND_EVENT --from-beginning
-./kafka-console-consumer --bootstrap-server kafka:29092 --topic PARKING_LOT --from-beginning
+./kafka-console-consumer --bootstrap-server kafka:29092 --topic PLAY_ODD_OR_EVEN_TOPIC --from-beginning
+./kafka-console-consumer --bootstrap-server kafka:29092 --topic ODD_TOPIC --from-beginning
+./kafka-console-consumer --bootstrap-server kafka:29092 --topic EVEN_TOPIC --from-beginning
