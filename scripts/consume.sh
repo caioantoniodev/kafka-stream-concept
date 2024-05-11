55df6f98fc593bc2db03f2e66e8d315396f08c5a
Caio Antonio <caio.antonio.c@outlook.com>
2024-05-10 21:01:14 -0300

    improvements

--- scripts/consume.sh
+++ scripts/consume.sh
@@ -1,9 +1,9 @@
 #outside container
-kcat -C -b localhost:9092 -t PLAY_ODD_OR_EVEN_TOPIC -e | jq
-kcat -C -b localhost:9092 -t ODD_TOPIC -e | jq
-kcat -C -b localhost:9092 -t EVEN_TOPIC -e | jq
+kcat -C -b localhost:9092 -t kafka-stream-concept.odd-or-event.play.v1.dev -e | jq
+kcat -C -b localhost:9092 -t kafka-stream-concept.odd.number.v1.dev -e | jq
+kcat -C -b localhost:9092 -t kafka-stream-concept.even.number.v1.dev -e | jq
 
 #inside container
-./kafka-console-consumer --bootstrap-server kafka:29092 --topic PLAY_ODD_OR_EVEN_TOPIC --from-beginning
-./kafka-console-consumer --bootstrap-server kafka:29092 --topic ODD_TOPIC --from-beginning
-./kafka-console-consumer --bootstrap-server kafka:29092 --topic EVEN_TOPIC --from-beginning
+./kafka-console-consumer --bootstrap-server kafka:29092 --topic kafka-stream-concept.odd-or-even.play.v1.dev --from-beginning
+./kafka-console-consumer --bootstrap-server kafka:29092 --topic kafka-stream-concept.odd.number.v1.dev --from-beginning
+./kafka-console-consumer --bootstrap-server kafka:29092 --topic kafka-stream-concept.even.number.v1.dev --from-beginning
--- scripts/produce.sh
+++ scripts/produce.sh
@@ -1,5 +1,5 @@
 #outside container
-echo '{"number": 3}' | kcat -P -b localhost:9092 -t PLAY_ODD_OR_EVEN_TOPIC
+echo '{"number": 3}' | kcat -P -b localhost:9092 -t kafka-stream-concept.odd-or-even.play.v1.dev
 
 #inside container
-echo '{"number": 3}' | ./kafka-console-producer --bootstrap-server kafka:9092 --topic PLAY_ODD_OR_EVEN_TOPIC
+echo '{"number": 3}' | ./kafka-console-producer --bootstrap-server kafka:9092 --topic kafka-stream-concept.odd-or-even.play.v1.dev
